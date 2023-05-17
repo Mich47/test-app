ecddd3e73ccd535d946e76f84483215a5ad47a56
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-18 01:50:03 +0300

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,6 +1,6 @@
 export const Description = () => {
   return (
-    <div className="py-[10px] mx-auto flex flex-col gap-4">
+    <div className="py-[10px] mx-auto flex flex-col gap-4 items-center">
       <h3 className="font-medium uppercase text-white text-xl">777 tweets</h3>
       <h3 className="font-medium uppercase text-white text-xl">
         100,501 followers
