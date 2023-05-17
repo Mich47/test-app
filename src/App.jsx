ab64ec8a16214ce7e5fff18f1b295dc60ad7d9bf
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-18 01:51:18 +0300

    added: Routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import "./App.css";
+// import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { Card } from "./components/Card";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Card />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="tweets" element={<Card />} />
+        </Route>
+      </Routes>
+      {/* <Card /> */}
     </>
   );
 }
