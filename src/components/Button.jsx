0cda6cf762e610f8c9f3c1ba0ac2b52bb9f9591e
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-17 23:47:23 +0300

    added: Button.jsx

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+export const Button = () => {
+  return (
+    <div className="py-4">
+      <button
+        className="rounded-[10px] py-[14px] w-[196px] shadow-btnShadow bg-white font-medium uppercase text-black text-lg leading-[22px]"
+        type="button"
+      >
+        Follow
+      </button>
+    </div>
+  );
+};
