8507d6e4e4bf36487316c75aa37bbf700ae6a993
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-17 23:47:56 +0300

    Update Line.jsx

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,7 +1,7 @@
-export const Line = () => {
+export const Line = ({ children }) => {
   return (
-    <div className="w-[380px] ml-[-20px] py-[10px] ">
-      <div className="h-2 shadow-lineShadow"></div>
+    <div className="w-[380px] ml-[-20px] pt-[10px] pb-[52px]">
+      <div className="relative h-2 shadow-lineShadow bg-white">{children}</div>
     </div>
   );
 };
