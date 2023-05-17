67ff5d4668116ea0611a42fa777ecb082f882959
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-17 23:46:55 +0300

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,14 @@
 import avatar from "../assets/avatar.svg";
+import { Line } from "./Line";
 
 export const Avatar = () => {
   return (
-    <div className="max-w-fit rounded-full mx-auto border-8 border-blue shadow-userShadow">
-      <img src={avatar} alt="avatar" />
-    </div>
+    <Line>
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 rounded-full mx-auto border-8 border-white bg-[#5735A4]">
+        <div className="w-20 h-20 rounded-full shadow-userShadow ml-[-8px] mt-[-8px] p-2">
+          <img className="w-full h-full" src={avatar} alt="avatar" />
+        </div>
+      </div>
+    </Line>
   );
 };
