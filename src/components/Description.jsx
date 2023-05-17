cdf40e295ec61abed7b39f2733916e81eb5844dc
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-17 23:47:41 +0300

    Create Description.jsx

---
+++ src/components/Description.jsx
@@ -0,0 +1,10 @@
+export const Description = () => {
+  return (
+    <div className="py-[10px] mx-auto flex flex-col gap-4">
+      <h3 className="font-medium uppercase text-white text-xl">777 tweets</h3>
+      <h3 className="font-medium uppercase text-white text-xl">
+        100,501 followers
+      </h3>
+    </div>
+  );
+};
