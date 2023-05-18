ba61c813a89ab8679931e17047b4eb484937cbbd
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-19 00:53:28 +0300

    Update Picture.jsx

--- src/components/Card/Picture.jsx
+++ src/components/Card/Picture.jsx
@@ -2,7 +2,7 @@ import image from "../../assets/picture.svg";
 
 export const Picture = () => {
   return (
-    <div className="py-1">
+    <div className="py-1 h-44">
       <img className="mx-auto" src={image} alt="image" />
     </div>
   );
