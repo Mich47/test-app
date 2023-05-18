b2c1f930f3b45382df18f64607cac1bc76dcb202
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-18 17:55:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="tweets" element={<Tweets />} />
+          <Route path="/tweets" element={<Tweets />} />
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
