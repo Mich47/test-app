e1a92e2af367c0f98a4b9391a411a7a5650c5f10
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-19 00:53:17 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 export const Button = ({ text = "Follow", handleClick, isFollow }) => {
   return (
     <button
-      className={`rounded-[10px] py-[14px] mx-auto w-[196px] shadow-btnShadow ${
+      className={`rounded-[10px] py-3.5 mx-auto w-[196px] shadow-btnShadow ${
         isFollow ? "bg-green" : "bg-white"
-      } font-medium uppercase text-black text-lg leading-[22px]`}
+      } font-medium font-semibold uppercase text-black text-lg leading-[22px]`}
       type="button"
       onClick={handleClick}
     >
