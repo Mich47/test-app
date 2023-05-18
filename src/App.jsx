de918e857f7f51d0e23816d4a5ba0aead627d976
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-19 00:53:11 +0300

    updated: added lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Tweets from "./pages/Tweets";
+import { Suspense, lazy } from "react";
 import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
-import Tweets from "./pages/Tweets";
+const Home = lazy(() => import("./pages/Home"));
+const Tweets = lazy(() => import("./pages/Tweets"));
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="tweets" element={<Tweets />} />
+          <Route
+            index
+            element={
+              // <Suspense fallback={<Loader />}>
+              <Suspense fallback="">
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="tweets"
+            element={
+              <Suspense fallback="">
+                <Tweets />
+              </Suspense>
+            }
+          />
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
