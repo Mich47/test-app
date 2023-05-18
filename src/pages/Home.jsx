df05cb04acabb7ee4331e51a0b3fb9f91c757e2d
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-18 22:11:37 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
       <div className="text-center">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Tweets test
+          Tweets
         </h1>
         <p className="mt-6 text-lg leading-8 text-gray-600">
           Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
