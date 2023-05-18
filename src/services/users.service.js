5a484925fca680975106acd1ac3557cd00c8d678
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-18 17:45:58 +0300

    Create users.service.js

---
+++ src/services/users.service.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const usersApi = axios.create({
+  baseURL: "https://63e7d624cbdc5658737e62f9.mockapi.io/users",
+});
+
+export const getUsers = async (params = {}) => {
+  const { data } = await usersApi.get("", {
+    params: { ...params },
+  });
+
+  console.log("data ", data);
+  return data;
+};
