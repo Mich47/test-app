6db2baf168dfa22de13942ca2653d315550a4319
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-19 14:59:35 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
-export const Button = ({ text = "Follow", handleClick, isFollow }) => {
+export const Button = ({ text = "Follow", onClickFn, isFollow }) => {
   return (
     <button
       className={`rounded-[10px] py-3.5 mx-auto w-[196px] shadow-btnShadow ${
         isFollow ? "bg-green" : "bg-white"
       } font-medium font-semibold uppercase text-black text-lg leading-[22px]`}
       type="button"
-      onClick={handleClick}
+      onClick={onClickFn}
     >
       {text}
     </button>
