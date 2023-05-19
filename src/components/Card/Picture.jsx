19dcf2f7e02dc159d10f7f45d3f7a76446595318
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-19 15:00:13 +0300

    Update Picture.jsx

--- src/components/Card/Picture.jsx
+++ src/components/Card/Picture.jsx
@@ -1,9 +1,17 @@
-import image from "../../assets/picture.svg";
+import image from "../../assets/images/picture.png";
+import imageWebp from "../../assets/images/picture.webp";
 
 export const Picture = () => {
   return (
     <div className="py-1 h-44">
-      <img className="mx-auto" src={image} alt="image" />
+      <picture>
+        <source srcSet={imageWebp} type="image/webp" />
+        <img
+          className="mx-auto max-h-full"
+          src={image}
+          alt="background-image"
+        />
+      </picture>
     </div>
   );
 };
