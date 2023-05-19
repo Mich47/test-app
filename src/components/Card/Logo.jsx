b5a2e9bea8fd3125cfed4e6ad9a54a60614a86b5
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-19 14:59:52 +0300

    Update Logo.jsx

--- src/components/Card/Logo.jsx
+++ src/components/Card/Logo.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/images/logo.svg";
 
 export const Logo = () => {
   return <img className="absolute left-5 top-5" src={logo} alt="logo" />;
