6421b48573a3d5f2544544fd19a47128616fafbf
Dmytro Muzyka <mmitya889@gmail.com>
2023-05-21 00:50:46 +0300

    Create arrow-down.svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ export default {
       backgroundImage: {
         "blue-gradient":
           "linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)",
+        "arrow-down": "url('./assets/images/arrow-down.svg')",
       },
       boxShadow: {
         lineShadow:
