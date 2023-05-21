5657e795f6a4b619173aea6abd2f8a7d87f3bc95
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-21 11:33:53 +0300

    updated: rename and update ButtonLoadMore

---
+++ src/pages/Tweets/Button.jsx
@@ -0,0 +1,14 @@
+import { STATUS } from "../../constants/status.constants";
+
+export const Button = ({ status, text, onClickFn }) => {
+  return (
+    <button
+      disabled={status === STATUS.loading}
+      type="button"
+      onClick={onClickFn}
+      className="rounded-lg bg-indigo-600 px-8 py-2.5 text-lg font-semibold font-medium text-white shadow-btnShadow hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors"
+    >
+      {text}
+    </button>
+  );
+};
