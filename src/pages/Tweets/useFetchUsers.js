8963fca0b498758e183f157ef68ad448125043b7
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-21 14:28:31 +0300

    updated: added save to LS

--- src/pages/Tweets/useFetchUsers.js
+++ src/pages/Tweets/useFetchUsers.js
@@ -1,22 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getUsers } from "../../services/users.service";
 import { STATUS } from "../../constants/status.constants";
 import { autoScrollBottom } from "../../utils/autoScrollBottom";
 
 export const useFetchUsers = () => {
+  const LIMIT_PER_PAGE = 3;
   const [page, setPage] = useState(1);
   const [users, setUsers] = useState([]);
   const [isMoreUsers, setIsMoreUsers] = useState(true);
   const [status, setStatus] = useState(STATUS.idle);
+  const firstRun = useRef(true);
 
   useEffect(() => {
+    if (firstRun.current) {
+      firstRun.current = false;
+      try {
+        const usersInLS = JSON.parse(localStorage.getItem("users"));
+
+        if (usersInLS && usersInLS.length) {
+          console.log("usersInLS.length ", usersInLS.length);
+          const pageInLs =
+            usersInLS.length % LIMIT_PER_PAGE
+              ? 1
+              : usersInLS.length / LIMIT_PER_PAGE;
+          setUsers(usersInLS);
+          setPage(pageInLs);
+          return;
+        }
+      } catch (error) {
+        console.log("error ", error);
+      }
+    }
+
     fetchData(page);
-  }, [page]);
+  }, [page, firstRun]);
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
 
   const fetchData = async (page = 1) => {
+    console.log("page ", page);
+    console.log("fetchData ");
     const params = {
       page,
-      limit: 3,
+      limit: LIMIT_PER_PAGE,
     };
 
     try {
@@ -32,6 +60,8 @@ export const useFetchUsers = () => {
       if (data.length < params.limit) {
         setIsMoreUsers(false);
       }
+
+      // localStorage.setItem("users", JSON.stringify([...users, ...data]));
 
       setUsers((prev) => [...prev, ...data]);
 
